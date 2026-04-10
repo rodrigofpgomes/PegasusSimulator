5293df5c8fe78706ef71d648dfc18c69d8c16b0f
rodrigofpgomes <rodrigofpgomes@gmail.com>
2026-04-10 17:50:24 +0100

    feat: add PPO and SAC support in __init__

--- extensions/pegasus.simulator/pegasus/simulator/logic/rl/algorithms/__init__.py
+++ extensions/pegasus.simulator/pegasus/simulator/logic/rl/algorithms/__init__.py
@@ -3,16 +3,11 @@ RL Algorithms
 
 Available algorithms:
 - ppo: Proximal Policy Optimization
-- sac: Soft Actor-Critic (future)
+- sac: Soft Actor-Critic
 - td3: Twin Delayed DDPG (future)
-
-Each algorithm has:
-- runner.py: Main training logic
-- __init__.py: Exports train() function
 """
 
 from . import ppo
-# from . import sac  # Adicionar quando implementar
-# from . import td3  # Adicionar quando implementar
+from . import sac
 
-__all__ = ["ppo"]
+__all__ = ["ppo", "sac"]
